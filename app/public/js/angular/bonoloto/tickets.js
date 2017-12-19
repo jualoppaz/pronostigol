a460fa2ce2ef4244acd16d4a89eac300855a4a42
Juanma <juanmalp1992@gmail.com>
2017-12-19 21:58:21 +0100

    #75 Modificación de consulta de tickets de Bonoloto por año en el front tras reunificar recurso en el GET /bonoloto/tickets.

--- app/public/js/angular/bonoloto/tickets.js
+++ app/public/js/angular/bonoloto/tickets.js
@@ -13,30 +13,34 @@ app.controller('TicketsController', function ($scope, $http, $window, $filter, V
 
     $scope.mostrarTickets = function(anyo){
         if($scope.tickets.length == 0 || $scope.tickets[0].anyo != anyo){
-            $http.get('/api/bonoloto/tickets/anyo/' + anyo)
-                .success(function(data){
-                    $scope.tickets = data;
-                    $scope.totalItems = data.length;
-                    $scope.numOfPages = data.length / $scope.ticketsPerPage;
+            $http.get('/api/bonoloto/tickets', {
+                params: {
+                    year: anyo
+                }
+            })
+            .success(function(data){
+                $scope.tickets = data;
+                $scope.totalItems = data.length;
+                $scope.numOfPages = data.length / $scope.ticketsPerPage;
 
-                    console.log("Numero de paginas: " + $scope.numOfPages);
+                console.log("Numero de paginas: " + $scope.numOfPages);
 
-                    var floor = Math.floor(data.length / $scope.ticketsPerPage);
+                var floor = Math.floor(data.length / $scope.ticketsPerPage);
 
-                    if($scope.numOfPages > floor){
-                        $scope.numOfPages = Math.floor(data.length / $scope.ticketsPerPage) + 1;
-                    }
+                if($scope.numOfPages > floor){
+                    $scope.numOfPages = Math.floor(data.length / $scope.ticketsPerPage) + 1;
+                }
 
-                    $scope.paginas = [];
+                $scope.paginas = [];
 
-                    for(var i=0; i<$scope.numOfPages; i++){
-                        $scope.paginas[i] = i+1;
-                    }
+                for(var i=0; i<$scope.numOfPages; i++){
+                    $scope.paginas[i] = i+1;
+                }
 
-                })
-                .error(function(data){
-                    console.log(data);
-                });
+            })
+            .error(function(data){
+                console.log(data);
+            });
         }
     };
 
