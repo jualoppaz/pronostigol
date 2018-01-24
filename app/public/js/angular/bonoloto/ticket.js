27a3e7698cefad5b59bec38d6c5e52feb876a99d
Juanma <juanmalp1992@gmail.com>
2018-01-24 18:29:27 +0100

    #177 Centralización del cálculo de número de aciertos y aspectos relacionados en el servicio "bonoloto".

--- app/public/js/angular/bonoloto/ticket.js
+++ app/public/js/angular/bonoloto/ticket.js
@@ -30,184 +30,22 @@ function Controller ($scope, $http, $filter, VariosService, bonoloto) {
         });
 
     $scope.determinarCategoriaPremio = function (combinacion) {
-        var res = "";
-        var resultado = $scope.ticket.resultado;
-        var numeroAciertos = 0;
-        var reintegroAcertado = false;
-        var complementarioAcertado = false;
-
-        if ($scope.ticket.apuestas.reintegro == resultado.reintegro) { // 0<=R<=9. Por tanto, podemos tener cualquier categoría.
-            var reintegroAcertado = true;
-
-            for (i = 0; i < combinacion.length; i++) {
-                for (j = 0; j < resultado.bolas.length; j++) {
-                    if (combinacion[i].numero == resultado.bolas[j].numero) {
-                        numeroAciertos += 1;
-                    }
-                }
-            }
-
-            if (numeroAciertos == 5) {
-                for (i = 0; i < combinacion.length; i++) {
-                    for (j = 0; j < resultado.bolas.length; j++) {
-                        if (combinacion[i].numero == resultado.complementario) {
-                            complementarioAcertado = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-        } else { // Se puede acertar 6, 5, 4 o 3
-            for (i = 0; i < combinacion.length; i++) {
-                for (j = 0; j < resultado.bolas.length; j++) {
-                    if (combinacion[i].numero == resultado.bolas[j].numero) {
-                        numeroAciertos += 1;
-                    }
-                }
-            }
-
-            if (numeroAciertos == 5) {
-                for (var i = 0; i < combinacion.length; i++) {
-                    for (var j = 0; j < resultado.bolas.length; j++) {
-                        if (combinacion[i].numero == resultado.complementario) {
-                            complementarioAcertado = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (numeroAciertos == 6) {
-            res = "6"
-        } else if (numeroAciertos == 5) {
-            if (complementarioAcertado) {
-                res = "5 + C";
-            } else {
-                res = "5";
-            }
-        } else if (numeroAciertos == 4) {
-            res = "4";
-        } else if (numeroAciertos == 3) {
-            res = "3";
-        } else if (reintegroAcertado) {
-            res = "R";
-        } else {
-            res = "-";
-        }
-
-        return res;
-
+        return bonoloto.getPrizeCategory($scope.ticket.resultado, combinacion, $scope.ticket.apuestas.reintegro);
     };
 
     $scope.determinarNumeroAciertos = function (combinacion) {
-
-        var res = "";
-
-        var resultado = $scope.ticket.resultado;
-
-        var numeroAciertos = 0;
-
-        var reintegroAcertado = false;
-
-        var complementarioAcertado = false;
-
-        if ($scope.ticket.apuestas.reintegro == resultado.reintegro) { // 0<=R<=9. Por tanto, podemos tener cualquier categoría.
-            var reintegroAcertado = true;
-
-            for (i = 0; i < combinacion.length; i++) {
-                for (j = 0; j < resultado.bolas.length; j++) {
-                    if (combinacion[i].numero == resultado.bolas[j].numero) {
-                        numeroAciertos += 1;
-                    }
-                }
-            }
-
-            if (numeroAciertos == 5) {
-                for (i = 0; i < combinacion.length; i++) {
-                    for (j = 0; j < resultado.bolas.length; j++) {
-                        if (combinacion[i].numero == resultado.complementario) {
-                            complementarioAcertado = true;
-                            break;
-                        }
-                    }
-                }
-            }
-
-        } else { // Se puede acertar 6, 5, 4 o 3
-            for (i = 0; i < combinacion.length; i++) {
-                for (j = 0; j < resultado.bolas.length; j++) {
-                    if (combinacion[i].numero == resultado.bolas[j].numero) {
-                        numeroAciertos += 1;
-                    }
-                }
-            }
-
-            if (numeroAciertos == 5) {
-                for (var i = 0; i < combinacion.length; i++) {
-                    for (var j = 0; j < resultado.bolas.length; j++) {
-                        if (combinacion[i].numero == resultado.complementario) {
-                            complementarioAcertado = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (numeroAciertos == 6) {
-            res = "6";
-        } else if (numeroAciertos == 5) {
-            if (complementarioAcertado) {
-                res = "5 + C";
-            } else {
-                res = "5";
-            }
-        } else if (numeroAciertos == 4) {
-            res = "4";
-        } else if (numeroAciertos == 3) {
-            res = "3";
-        } else if (numeroAciertos == 2) {
-            res = "2";
-        } else if (numeroAciertos == 1) {
-            res = "1";
-        } else if (reintegroAcertado) {
-            res = "R";
-        } else {
-            res = "0";
-        }
-
-        return res;
-
+        return bonoloto.getSuccessfulNumbersAmount($scope.ticket.resultado, combinacion, $scope.ticket.apuestas.reintegro);
     };
 
     $scope.bolaHaSidoAcertada = function (bola) {
-        var resultado = $scope.ticket.resultado;
-        var res = false;
-
-        for (var i = 0; i < resultado.bolas.length; i++) {
-            if (bola.numero == resultado.bolas[i].numero) {
-                res = true;
-                break;
-            }
-        }
-
-        return res;
+        return bonoloto.isSuccessfulNumber($scope.ticket.resultado, bola);
     };
 
     $scope.bolaHaSidoAcertadaComoComplementario = function (bola) {
-
-        var resultado = $scope.ticket.resultado;
-
-        var res = false;
-
-        res = bola.numero == resultado.complementario;
-
-        return res;
+        return bonoloto.isSuccessfulNumberAsComplementary($scope.ticket.resultado, bola);
     };
 
     $scope.ticketEstaVacio = function () {
         return VariosService.jsonVacio($scope.ticket);
     };
-};
+}
--- app/public/js/angular/services/bonoloto.js
+++ app/public/js/angular/services/bonoloto.js
@@ -12,6 +12,11 @@
         var apiPrefix = "/api/bonoloto";
 
         var service = {
+            // Utils
+            getPrizeCategory: getPrizeCategory,
+            getSuccessfulNumbersAmount: getSuccessfulNumbersAmount,
+            isSuccessfulNumber: isSuccessfulNumber,
+            isSuccessfulNumberAsComplementary: isSuccessfulNumberAsComplementary,
             // Tickets
             getTickets: getTickets,
             getTicketById: getTicketById,
@@ -32,6 +37,166 @@
         };
 
         return service;
+
+        /**
+         * Metodo para determinar la categoria del premio asociada a una apuesta dados el resultado, apuesta y reintegro apostado
+         */
+        function getPrizeCategory(resultado, combinacion, reintegroApostado){
+            var res = "";
+            var numeroAciertos = 0;
+            var reintegroAcertado = false;
+            var complementarioAcertado = false;
+
+            if (reintegroApostado === resultado.reintegro) { // 0<=R<=9. Por tanto, podemos tener cualquier categoría.
+                reintegroAcertado = true;
+
+                for (i = 0; i < combinacion.length; i++) {
+                    for (j = 0; j < resultado.bolas.length; j++) {
+                        if (combinacion[i].numero === resultado.bolas[j].numero) {
+                            numeroAciertos += 1;
+                        }
+                    }
+                }
+
+                if (numeroAciertos === 5) {
+                    for (i = 0; i < combinacion.length; i++) {
+                        for (j = 0; j < resultado.bolas.length; j++) {
+                            if (combinacion[i].numero === resultado.complementario) {
+                                complementarioAcertado = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            } else { // Se puede acertar 6, 5, 4 o 3
+                for (i = 0; i < combinacion.length; i++) {
+                    for (j = 0; j < resultado.bolas.length; j++) {
+                        if (combinacion[i].numero === resultado.bolas[j].numero) {
+                            numeroAciertos += 1;
+                        }
+                    }
+                }
+
+                if (numeroAciertos === 5) {
+                    for (var i = 0; i < combinacion.length; i++) {
+                        for (var j = 0; j < resultado.bolas.length; j++) {
+                            if (combinacion[i].numero === resultado.complementario) {
+                                complementarioAcertado = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (numeroAciertos === 6) {
+                res = "6"
+            } else if (numeroAciertos === 5) {
+                if (complementarioAcertado) {
+                    res = "5 + C";
+                } else {
+                    res = "5";
+                }
+            } else if (numeroAciertos === 4) {
+                res = "4";
+            } else if (numeroAciertos === 3) {
+                res = "3";
+            } else if (reintegroAcertado) {
+                res = "R";
+            } else {
+                res = "-";
+            }
+
+            return res;
+        }
+
+        /**
+         * Metodo para determinar el número de aciertos en una apuesta
+         */
+        function getSuccessfulNumbersAmount(resultado, combinacion, reintegroApostado){
+            var res = "";
+
+            var numeroAciertos = 0;
+
+            var reintegroAcertado = false;
+
+            var complementarioAcertado = false;
+
+            if (reintegroApostado === resultado.reintegro) { // 0<=R<=9. Por tanto, podemos tener cualquier categoría.
+                reintegroAcertado = true;
+            }
+
+            // Se puede acertar 6, 5, 4 o 3
+            for (var i = 0; i < combinacion.length; i++) {
+                for (var j = 0; j < resultado.bolas.length; j++) {
+                    if (combinacion[i].numero === resultado.bolas[j].numero) {
+                        numeroAciertos += 1;
+                    }
+
+                    if (combinacion[i].numero === resultado.complementario) {
+                        complementarioAcertado = true;
+                    }
+                }
+            }
+
+            if (numeroAciertos !== 5) {
+                complementarioAcertado = false;
+            }
+
+            if (numeroAciertos === 6) {
+                res = "6";
+            } else if (numeroAciertos === 5) {
+                if (complementarioAcertado) {
+                    res = "5 + C";
+                } else {
+                    res = "5";
+                }
+            } else if (numeroAciertos === 4) {
+                res = "4";
+            } else if (numeroAciertos === 3) {
+                res = "3";
+            } else if (numeroAciertos === 2) {
+                res = "2";
+            } else if (numeroAciertos === 1) {
+                res = "1";
+            } else if (reintegroAcertado) {
+                res = "R";
+            } else {
+                res = "0";
+            }
+
+            return res;
+        }
+
+        /**
+         * Metodo para determinar si un número incluido en una apuesta ha aparecido en la apuesta ganadora
+         */
+        function isSuccessfulNumber(resultado, bola){
+            var res = false;
+
+            for (var i = 0; i < resultado.bolas.length; i++) {
+                if (String(bola.numero) === String(resultado.bolas[i].numero)) {
+                    res = true;
+                    break;
+                }
+            }
+
+            return res;
+        }
+
+        /**
+         * Metodo para determinar si un número incluido en una apuesta coincide con el complementario de la apuesta ganadora
+         */
+        function isSuccessfulNumberAsComplementary(resultado, bola){
+            var res = false;
+
+            if (String(bola.numero) === String(resultado.complementario)) {
+                res = true;
+            }
+
+            return res;
+        }
 
         function getTickets(queryParameters){
             var defered = $q.defer();
