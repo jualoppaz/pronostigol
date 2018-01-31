5d387f6d43456732160a2181e13f639ccf246d96
Juanma <juanmalp1992@gmail.com>
2018-01-31 17:18:27 +0100

    #194 Implementación de consulta de tickets de Euromillones durante la edición de un ticket de Euromillones.

--- app/public/js/angular/admin/euromillones/editarTicket.js
+++ app/public/js/angular/admin/euromillones/editarTicket.js
@@ -26,15 +26,13 @@ function Controller($scope, $http, $window, $filter, euromillones){
             console.log(err);
         });
 
-    $scope.anyos = [
-        {
-            name: "2014",
-            value: "2014"
-        },{
-            name: "2015",
-            value: "2015"
-        }
-    ];
+    euromillones.getAllYears()
+        .then(function(data){
+            $scope.anyos = data;
+        })
+        .catch(function(err){
+            console.log(err);
+        });
 
     $scope.ticket.resultado = {
         bolas: [
