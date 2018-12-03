891d07fbc6d76e84bd9265461419312cab71093a
Juanma <juanmalp1992@gmail.com>
2018-12-03 19:11:17 +0100

    #254 Actualización de apuestas por defecto en la creación de un ticket de la Primitiva.

--- app/public/js/angular/admin/primitiva/anadirTicket.js
+++ app/public/js/angular/admin/primitiva/anadirTicket.js
@@ -1,22 +1,22 @@
-var app = angular.module('dashboard');
+var app = angular.module("dashboard");
 
-app.controller('TicketController', Controller);
+app.controller("TicketController", Controller);
 
-Controller.$inject = ['$scope', '$http', '$window', '$filter', 'primitiva'];
+Controller.$inject = ["$scope", "$http", "$window", "$filter", "primitiva"];
 
-function Controller ($scope, $http, $window, $filter, primitiva){
-
+function Controller($scope, $http, $window, $filter, primitiva) {
     $scope.ticket = {};
 
-    $scope.ticket.precio = "2";
+    $scope.ticket.precio = "4";
 
     $scope.anyos = [];
 
-    primitiva.getAllYears()
-        .then(function(data){
-            $scope.anyos = $filter('orderBy')(data, "name");
+    primitiva
+        .getAllYears()
+        .then(function (data) {
+            $scope.anyos = $filter("orderBy")(data, "name");
         })
-        .catch(function(err){
+        .catch(function (err) {
             console.log(err);
         });
 
@@ -24,15 +24,20 @@ function Controller ($scope, $http, $window, $filter, primitiva){
         bolas: [
             {
                 numero: ""
-            },{
+            },
+            {
                 numero: ""
-            },{
+            },
+            {
                 numero: ""
-            },{
+            },
+            {
                 numero: ""
-            },{
+            },
+            {
                 numero: ""
-            },{
+            },
+            {
                 numero: ""
             }
         ],
@@ -46,118 +51,183 @@ function Controller ($scope, $http, $window, $filter, primitiva){
             [
                 {
                     numero: "4"
-                },{
+                },
+                {
                     numero: "8"
-                },{
+                },
+                {
                     numero: "13"
-                },{
+                },
+                {
                     numero: "19"
-                },{
+                },
+                {
                     numero: "27"
-                },{
+                },
+                {
                     numero: "38"
                 }
-            ], [
+            ],
+            [
                 {
                     numero: "1"
-                },{
+                },
+                {
                     numero: "6"
-                },{
+                },
+                {
                     numero: "10"
-                },{
+                },
+                {
                     numero: "22"
-                },{
+                },
+                {
                     numero: "37"
-                },{
+                },
+                {
                     numero: "46"
+                }
+            ],
+            [
+                {
+                    numero: "32"
+                },
+                {
+                    numero: "36"
+                },
+                {
+                    numero: "39"
+                },
+                {
+                    numero: "42"
+                },
+                {
+                    numero: "45"
+                },
+                {
+                    numero: "47"
+                }
+            ],
+            [
+                {
+                    numero: "40"
+                },
+                {
+                    numero: "41"
+                },
+                {
+                    numero: "43"
+                },
+                {
+                    numero: "44"
+                },
+                {
+                    numero: "48"
+                },
+                {
+                    numero: "49"
                 }
             ]
         ]
     };
 
-    $scope.anadirApuesta = function(){
-        if($scope.ticket.apuestas.combinaciones == null){
-
+    $scope.anadirApuesta = function () {
+        if ($scope.ticket.apuestas.combinaciones == null) {
             $scope.ticket.apuestas.combinaciones = [
                 [
                     {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
                     }
                 ]
             ];
-        }else if($scope.ticket.apuestas.combinaciones.length === 0){
+        } else if ($scope.ticket.apuestas.combinaciones.length === 0) {
             $scope.ticket.apuestas.combinaciones = [
                 [
                     {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
-                    },{
+                    },
+                    {
                         numero: ""
                     }
                 ]
             ];
         }
 
-        if($scope.ticket.apuestas.combinaciones.length < 8){
-            $scope.ticket.apuestas.combinaciones[$scope.ticket.apuestas.combinaciones.length] = [
-                {
-                    numero: ""
-                }, {
-                    numero: ""
-                }, {
-                    numero: ""
-                }, {
-                    numero: ""
-                }, {
-                    numero: ""
-                }, {
-                    numero: ""
-                }
-            ];
+        if ($scope.ticket.apuestas.combinaciones.length < 8) {
+            $scope.ticket.apuestas.combinaciones[
+                $scope.ticket.apuestas.combinaciones.length
+            ] = [
+                    {
+                        numero: ""
+                    },
+                    {
+                        numero: ""
+                    },
+                    {
+                        numero: ""
+                    },
+                    {
+                        numero: ""
+                    },
+                    {
+                        numero: ""
+                    },
+                    {
+                        numero: ""
+                    }
+                ];
         }
     };
 
-    $scope.eliminarApuesta = function(){
-
-        if($scope.ticket.apuestas.combinaciones.length !== 0){
-
+    $scope.eliminarApuesta = function () {
+        if ($scope.ticket.apuestas.combinaciones.length !== 0) {
             $scope.ticket.apuestas.combinaciones.pop();
 
-            if($scope.ticket.apuestas.combinaciones.length === 1){
+            if ($scope.ticket.apuestas.combinaciones.length === 1) {
                 $scope.ticket.apuestas.combinaciones = [];
             }
-
         }
     };
 
-    $scope.guardar = function(){
-        primitiva.createTicket($scope.ticket)
-            .then(function(){
+    $scope.guardar = function () {
+        primitiva
+            .createTicket($scope.ticket)
+            .then(function () {
                 $scope.redirigir();
             })
-            .catch(function(err){
+            .catch(function (err) {
                 console.log(err);
             });
     };
 
-    $scope.redirigir = function(){
+    $scope.redirigir = function () {
         $window.location.href = "/admin/primitiva";
     };
 }
