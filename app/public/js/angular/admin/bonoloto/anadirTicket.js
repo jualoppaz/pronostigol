baa3b3285218ea49ddd6d746bc32a2b75a53e015
Juanma <juanmalp1992@gmail.com>
2018-12-03 18:39:20 +0100

    #253 Actualización de apuestas por defecto en la Bonoloto.

--- app/public/js/angular/admin/bonoloto/anadirTicket.js
+++ app/public/js/angular/admin/bonoloto/anadirTicket.js
@@ -1,161 +1,233 @@
-var app = angular.module('dashboard');
+var app = angular.module("dashboard");
 
-app.controller('TicketController', Controller);
+app.controller("TicketController", Controller);
 
-Controller.$inject = ['$scope', '$http', '$window', '$filter', 'bonoloto'];
+Controller.$inject = ["$scope", "$http", "$window", "$filter", "bonoloto"];
 
 function Controller($scope, $http, $window, $filter, bonoloto) {
+  $scope.ticket = {};
 
-    $scope.ticket = {};
+  $scope.ticket.precio = "1";
 
-    $scope.ticket.precio = "1";
+  $scope.anyos = [];
 
-    $scope.anyos = [];
+  bonoloto
+    .getYears()
+    .then(function(data) {
+      $scope.anyos = $filter("orderBy")(data, "name");
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 
-    bonoloto.getYears()
-        .then(function (data) {
-            $scope.anyos = $filter('orderBy')(data, "name");
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+  $scope.ticket.resultado = {
+    bolas: [
+      {
+        numero: null
+      },
+      {
+        numero: null
+      },
+      {
+        numero: null
+      },
+      {
+        numero: null
+      },
+      {
+        numero: null
+      },
+      {
+        numero: null
+      }
+    ],
+    reintegro: null,
+    complementario: null
+  };
 
-    $scope.ticket.resultado = {
-        bolas: [
-            {
-                numero: null
-            }, {
-                numero: null
-            }, {
-                numero: null
-            }, {
-                numero: null
-            }, {
-                numero: null
-            }, {
-                numero: null
-            }
-        ],
-        reintegro: null,
-        complementario: null
-    };
+  $scope.ticket.apuestas = {
+    reintegro: null,
+    combinaciones: [
+      [
+        {
+          numero: 4
+        },
+        {
+          numero: 8
+        },
+        {
+          numero: 13
+        },
+        {
+          numero: 19
+        },
+        {
+          numero: 27
+        },
+        {
+          numero: 38
+        }
+      ],
+      [
+        {
+          numero: 1
+        },
+        {
+          numero: 6
+        },
+        {
+          numero: 10
+        },
+        {
+          numero: 22
+        },
+        {
+          numero: 37
+        },
+        {
+          numero: 46
+        }
+      ],
+      [
+        {
+          numero: 32
+        },
+        {
+          numero: 36
+        },
+        {
+          numero: 39
+        },
+        {
+          numero: 42
+        },
+        {
+          numero: 45
+        },
+        {
+          numero: 47
+        }
+      ],
+      [
+        {
+          numero: 40
+        },
+        {
+          numero: 41
+        },
+        {
+          numero: 43
+        },
+        {
+          numero: 44
+        },
+        {
+          numero: 48
+        },
+        {
+          numero: 49
+        }
+      ]
+    ]
+  };
 
-    $scope.ticket.apuestas = {
-        reintegro: null,
-        combinaciones: [
-            [
-                {
-                    numero: 4
-                }, {
-                    numero: 8
-                }, {
-                    numero: 13
-                }, {
-                    numero: 19
-                }, {
-                    numero: 27
-                }, {
-                    numero: 38
-                }
-            ], [
-                {
-                    numero: 1
-                }, {
-                    numero: 6
-                }, {
-                    numero: 10
-                }, {
-                    numero: 22
-                }, {
-                    numero: 37
-                }, {
-                    numero: 46
-                }
-            ]
+  $scope.anadirApuesta = function() {
+    if ($scope.ticket.apuestas.combinaciones == null) {
+      $scope.ticket.apuestas.combinaciones = [
+        [
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          }
         ]
-    };
+      ];
+    } else if ($scope.ticket.apuestas.combinaciones.length === 0) {
+      $scope.ticket.apuestas.combinaciones = [
+        [
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          },
+          {
+            numero: null
+          }
+        ]
+      ];
+    }
 
-    $scope.anadirApuesta = function () {
-        if ($scope.ticket.apuestas.combinaciones == null) {
-
-            $scope.ticket.apuestas.combinaciones = [
-                [
-                    {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }
-                ]
-            ];
-        } else if ($scope.ticket.apuestas.combinaciones.length === 0) {
-            $scope.ticket.apuestas.combinaciones = [
-                [
-                    {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }, {
-                        numero: null
-                    }
-                ]
-            ];
+    if ($scope.ticket.apuestas.combinaciones.length < 8) {
+      $scope.ticket.apuestas.combinaciones[
+        $scope.ticket.apuestas.combinaciones.length
+      ] = [
+        {
+          numero: null
+        },
+        {
+          numero: null
+        },
+        {
+          numero: null
+        },
+        {
+          numero: null
+        },
+        {
+          numero: null
+        },
+        {
+          numero: null
         }
+      ];
+    }
+  };
 
-        if ($scope.ticket.apuestas.combinaciones.length < 8) {
-            $scope.ticket.apuestas.combinaciones[$scope.ticket.apuestas.combinaciones.length] = [
-                {
-                    numero: null
-                }, {
-                    numero: null
-                }, {
-                    numero: null
-                }, {
-                    numero: null
-                }, {
-                    numero: null
-                }, {
-                    numero: null
-                }
-            ];
-        }
-    };
+  $scope.eliminarApuesta = function() {
+    if ($scope.ticket.apuestas.combinaciones.length !== 0) {
+      $scope.ticket.apuestas.combinaciones.pop();
 
-    $scope.eliminarApuesta = function () {
-        if ($scope.ticket.apuestas.combinaciones.length !== 0) {
+      if ($scope.ticket.apuestas.combinaciones.length === 1) {
+        $scope.ticket.apuestas.combinaciones = [];
+      }
+    }
+  };
 
-            $scope.ticket.apuestas.combinaciones.pop();
+  $scope.guardar = function() {
+    bonoloto
+      .createTicket($scope.ticket)
+      .then(function() {
+        $scope.redirigir();
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  };
 
-            if ($scope.ticket.apuestas.combinaciones.length === 1) {
-                $scope.ticket.apuestas.combinaciones = [];
-            }
-        }
-    };
-
-    $scope.guardar = function () {
-        bonoloto.createTicket($scope.ticket)
-            .then(function () {
-                $scope.redirigir();
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
-    };
-
-    $scope.redirigir = function () {
-        $window.location.href = "/admin/bonoloto";
-    };
+  $scope.redirigir = function() {
+    $window.location.href = "/admin/bonoloto";
+  };
 }
