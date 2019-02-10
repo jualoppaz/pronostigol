5a687fae2ca22f863e8c5e3179bd3427d1a4c852
Juanma <juanmalp1992@gmail.com>
2019-02-10 16:40:58 +0100

    #265 Implementación de paginación en recurso /gordo/historical/occurrencesBySpecialNumber.

--- app/server/modules/gordo-data-base-manager.js
+++ app/server/modules/gordo-data-base-manager.js
@@ -572,24 +572,75 @@ exports.getOccurrencesByResultWithoutSpecialNumber = function(
     });
 };
 
-exports.getOccurrencesBySpecialNumber = function(callback) {
-    gordo_tickets.aggregate(
-        {
-            $group: {
-                _id: "$resultado.numeroClave",
-                apariciones: {
-                    $sum: 1
-                }
-            }
-        },
-        function(e, res) {
-            if (e) {
-                callback(e);
-            } else {
-                callback(null, res);
+exports.getOccurrencesBySpecialNumber = function(filtros, callback) {
+    var limit = filtros.perPage;
+    var page = filtros.page;
+    var skip = (page - 1) * limit;
+    var sort = filtros.sort;
+    var type = filtros.type;
+
+    var sort_property =
+        sort === "specialNumber" ? "numeroClave" : "apariciones";
+    var sort_type = type === "asc" ? 1 : -1;
+
+    var query = [];
+    query.push({
+        $group: {
+            _id: "$resultado.numeroClave",
+            apariciones: {
+                $sum: 1
             }
         }
-    );
+    });
+
+    query.push({
+        $project: {
+            _id: 0,
+            numeroClave: "$_id",
+            apariciones: 1
+        }
+    });
+
+    gordo_tickets.aggregate(query, function(e, res) {
+        if (e) {
+            callback(e);
+        } else {
+            var result = {
+                page: page,
+                perPage: limit,
+                total: res.length
+            };
+
+            var sortConfig = {};
+            sortConfig[sort_property] = sort_type;
+
+            // Añadimos ordenación alternativa
+            if (sort_property === "apariciones") {
+                sortConfig["numeroClave"] = sort_type;
+            }
+
+            query.push({
+                $sort: sortConfig
+            });
+
+            query.push({
+                $skip: skip
+            });
+
+            query.push({
+                $limit: limit
+            });
+
+            gordo_tickets.aggregate(query, function(e, res) {
+                if (e) {
+                    callback(e);
+                } else {
+                    result.data = res;
+                    callback(null, result);
+                }
+            });
+        }
+    });
 };
 
 exports.getAllYears = function(callback) {
