159efb0e757b31318c5b9a58de6b441782f20b70
Juanma <juanmalp1992@gmail.com>
2019-03-31 23:10:29 +0200

    #356 Adaptación en el front tras haber renombrado los enlaces en el back.

--- app/public/js/angular/primitiva/tickets.js
+++ app/public/js/angular/primitiva/tickets.js
@@ -1,11 +1,17 @@
-var app = angular.module('qdb');
+var app = angular.module("qdb");
 
-app.controller('TicketsController', Controller);
+app.controller("TicketsController", Controller);
 
-Controller.$inject = ['$scope', '$http', '$window', '$filter', 'VariosService', 'primitiva'];
+Controller.$inject = [
+    "$scope",
+    "$http",
+    "$window",
+    "$filter",
+    "VariosService",
+    "primitiva"
+];
 
 function Controller($scope, $http, $window, $filter, VariosService, primitiva) {
-
     $scope.tickets = [];
 
     $scope.maxSize = 5;
@@ -13,27 +19,28 @@ function Controller($scope, $http, $window, $filter, VariosService, primitiva) {
     $scope.currentPage = 1;
     $scope.ticketsPerPage = 5;
 
-    primitiva.getAllYears()
-        .then(function (data) {
+    primitiva
+        .getAllYears()
+        .then(function(data) {
             $scope.years = data;
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log(err);
         });
 
-    $scope.mostrarTickets = function (anyo) {
+    $scope.mostrarTickets = function(anyo) {
         $scope.selected = {
             year: anyo
         };
 
         if ($scope.tickets.length === 0 || $scope.tickets[0].anyo != anyo) {
-
-            primitiva.getTickets({
-                year: anyo,
-                per_page: $scope.ticketsPerPage,
-                page: $scope.currentPage
-            })
-                .then(function (data) {
+            primitiva
+                .getTickets({
+                    year: anyo,
+                    per_page: $scope.ticketsPerPage,
+                    page: $scope.currentPage
+                })
+                .then(function(data) {
                     var tickets = data.data;
                     var total = data.total;
                     var perPage = data.perPage;
@@ -50,43 +57,42 @@ function Controller($scope, $http, $window, $filter, VariosService, primitiva) {
 
                     $scope.numOfPages = numOfPages;
                 })
-                .catch(function (err) {
+                .catch(function(err) {
                     console.log(err);
                 });
         }
     };
 
-    $scope.verTicket = function (ticket) {
-        $window.location.href = "/primitiva/tickets/" + ticket.anyo + "/" + ticket.sorteo;
+    $scope.verTicket = function(ticket) {
+        $window.location.href =
+            "/primitiva/sorteos/" + ticket.anyo + "/" + ticket.sorteo;
     };
 
-    $scope.traducirDia = function (fecha) {
-
-        var dia = $filter('date')(fecha, 'EEEE');
+    $scope.traducirDia = function(fecha) {
+        var dia = $filter("date")(fecha, "EEEE");
         return VariosService.traducirDia(dia);
     };
 
-
     // Paginacion manual
 
-    $scope.actualizarPagina = function (pagina) {
+    $scope.actualizarPagina = function(pagina) {
         $scope.currentPage = pagina;
     };
 
-    $scope.apuestaRealizada = function (ticket) {
+    $scope.apuestaRealizada = function(ticket) {
         return VariosService.apuestaRealizada(ticket);
     };
 
-    $scope.consultarTickets = function () {
-
+    $scope.consultarTickets = function() {
         $scope.tickets = [];
 
-        primitiva.getTickets({
-            year: $scope.selected.year,
-            per_page: $scope.ticketsPerPage,
-            page: $scope.currentPage
-        })
-            .then(function (data) {
+        primitiva
+            .getTickets({
+                year: $scope.selected.year,
+                per_page: $scope.ticketsPerPage,
+                page: $scope.currentPage
+            })
+            .then(function(data) {
                 var tickets = data.data;
                 var total = data.total;
                 var perPage = data.perPage;
@@ -103,9 +109,8 @@ function Controller($scope, $http, $window, $filter, VariosService, primitiva) {
 
                 $scope.numOfPages = numOfPages;
             })
-            .catch(function (err) {
+            .catch(function(err) {
                 console.log(err);
             });
-
     };
 }
