afd571417f473d93e1e452e70679f63f99d6c39c
Juanma <juanmalp1992@gmail.com>
2019-06-06 20:47:21 +0200

    #442 Redefinición de constantes de códigos HTTP.

--- app/server/constants/index.js
+++ app/server/constants/index.js
@@ -1,15 +1,7 @@
 var roles = require("./roles");
+var httpCodes = require("./httpCodes");
 
 module.exports = {
-    HTTP: {
-        OK: 200,
-        CREATED: 201,
-        UNAUTHORIZED: 401,
-        FORBIDDEN: 403,
-        NOT_FOUND: 404,
-        CONFLICT: 409,
-        UNPROCESSABLE_ENTITY: 422,
-        INTERNAL_SERVER_ERROR: 500
-    },
+    HTTP_CODES: httpCodes,
     ROLES: roles
 };
