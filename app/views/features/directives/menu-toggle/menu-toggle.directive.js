4401be3065f7d76c97de5f72d987eb3ce9767c56
Juanma <juanmalp1992@gmail.com>
2019-08-26 19:52:52 +0200

    #503 Modificación de animación de toggle por implementación pura con CSS mediante transiciones para evitar incidencia de angular-material.

--- app/views/features/directives/menu-toggle/menu-toggle.directive.js
+++ app/views/features/directives/menu-toggle/menu-toggle.directive.js
@@ -7,11 +7,6 @@ function MenuToggleDirective($mdUtil, $animateCss, $$rAF) {
         link: function($scope, $element) {
             var controller = $element.parent().controller();
 
-            // Used for toggling the visibility of the accordion's content, after
-            // all of the animations are completed. This prevents users from being
-            // allowed to tab through to the hidden content.
-            $scope.renderContent = false;
-
             $scope.isOpen = function() {
                 return controller.isOpen($scope.section);
             };
@@ -19,60 +14,6 @@ function MenuToggleDirective($mdUtil, $animateCss, $$rAF) {
             $scope.toggle = function() {
                 controller.toggleOpen($scope.section);
             };
-
-            $mdUtil.nextTick(function() {
-                $scope.$watch(
-                    function() {
-                        return controller.isOpen($scope.section);
-                    },
-                    function(open) {
-                        var $ul = $element.find("ul");
-                        var $li = $ul[0].querySelector("a.active");
-
-                        if (open) {
-                            $scope.renderContent = true;
-                        }
-
-                        $$rAF(function() {
-                            var targetHeight = open ? $ul[0].scrollHeight : 0;
-
-                            $animateCss($ul, {
-                                easing: "cubic-bezier(0.35, 0, 0.25, 1)",
-                                to: { height: targetHeight + "px" },
-                                duration: 0.75 // seconds
-                            })
-                                .start()
-                                .then(function() {
-                                    var $li = $ul[0].querySelector("a.active");
-
-                                    $scope.renderContent = open;
-
-                                    if (open && $li && $ul[0].scrollTop === 0) {
-                                        var activeHeight = $li.scrollHeight;
-                                        var activeOffset = $li.offsetTop;
-                                        var offsetParent = $li.offsetParent;
-                                        var parentScrollPosition = offsetParent
-                                            ? offsetParent.offsetTop
-                                            : 0;
-
-                                        // Reduce it a bit (2 list items' height worth) so it doesn't touch the nav
-                                        var negativeOffset = activeHeight * 2;
-                                        var newScrollTop =
-                                            activeOffset +
-                                            parentScrollPosition -
-                                            negativeOffset;
-
-                                        $mdUtil.animateScrollTo(
-                                            document.querySelector(".navs-menu")
-                                                .parentNode,
-                                            newScrollTop
-                                        );
-                                    }
-                                });
-                        });
-                    }
-                );
-            });
 
             var parentNode = $element[0].parentNode.parentNode.parentNode;
             if (parentNode.classList.contains("parent-list-item")) {
