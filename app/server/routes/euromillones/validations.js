c7ea89d43a92c7ffd287bc33b4d58f828ca0d5e0
Juanma <juanmalp1992@gmail.com>
2020-09-20 19:58:51 +0200

    #532 Implementación de validación del recurso GET /euromillones/historical/lastDateByStar

--- app/server/routes/euromillones/validations.js
+++ app/server/routes/euromillones/validations.js
@@ -81,5 +81,17 @@ module.exports = {
             sort_type: Joi.string().valid(["asc", "desc"]),
             sort_property: Joi.string().valid(["number", "date"])
         }
+    },
+    getLastDateByStar: {
+        query: {
+            page: Joi.number()
+                .min(1)
+                .required(),
+            per_page: Joi.number()
+                .min(1)
+                .required(),
+            sort_type: Joi.string().valid(["asc", "desc"]),
+            sort_property: Joi.string().valid(["star", "date"])
+        }
     }
 };
