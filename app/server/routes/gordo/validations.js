5f017efd8b7ebcf7bc3d8443e8a08edfb16771a1
Juanma <juanmalp1992@gmail.com>
2020-09-20 14:02:16 +0200

    #531 Implementación de validación del recurso GET /gordo/historical/lastDateBySpecialNumber

--- app/server/routes/gordo/validations.js
+++ app/server/routes/gordo/validations.js
@@ -70,4 +70,16 @@ module.exports = {
             sort_property: Joi.string().valid(["number", "date"])
         }
     },
+    getLastDateBySpecialNumber: {
+        query: {
+            page: Joi.number()
+                .min(1)
+                .required(),
+            per_page: Joi.number()
+                .min(1)
+                .required(),
+            sort_type: Joi.string().valid(["asc", "desc"]),
+            sort_property: Joi.string().valid(["specialNumber", "date"])
+        }
+    }
 };
