510b8996f3d4e47b81369961a711bc6993604154
Juanma <juanmalp1992@gmail.com>
2020-10-13 20:00:27 +0200

    #537 Implementación de análisis de equipo visitante en fila con todos los resultados, teniendo más victorias que empates y mismos empates que derrotas

--- app/public/js/angular/admin/quiniela/analizador.js
+++ app/public/js/angular/admin/quiniela/analizador.js
@@ -1466,24 +1466,24 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.visitorTeamMessages.push(message);
         }
 
-        /* message = quiniela.ANALYZER_MESSAGES.VISITOR.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.SAME;
+        message = quiniela.ANALYZER_MESSAGES.VISITOR.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.SAME;
 
-        if (fila.victoriasLocales > fila.empates && fila.empates === fila.victoriasVisitantes && fila.victoriasVisitantes > 0) {
+        if (fila.victoriasVisitantes > fila.empates && fila.empates === fila.victoriasLocales && fila.victoriasLocales > 0) {
             message = $scope.getCustomMessage(message, {
                 "{visitorTeam}": $scope.form.equipoVisitante,
                 '{row}': $scope.form.fila,
-                '{numWins}': sumaDeVictoriasLocalesComoVisitanteEnFila,
+                '{numWins}': sumaDeVictoriasVisitantesComoVisitanteEnFila,
                 '{numDraws}': sumaDeEmpatesComoVisitanteEnFila,
-                '{numLoses}': sumaDeVictoriasVisitantesComoVisitanteEnFila,
-                '{perWins}': porcentajeDeVictoriasLocalesComoVisitanteEnFila,
+                '{numLoses}': sumaDeVictoriasLocalesComoVisitanteEnFila,
+                '{perWins}': porcentajeDeVictoriasVisitantesComoVisitanteEnFila,
                 '{perDraws}': porcentajeDeEmpatesComoVisitanteEnFila,
-                '{perLoses}': porcentajeDeVictoriasVisitantesComoVisitanteEnFila,
+                '{perLoses}': porcentajeDeVictoriasLocalesComoVisitanteEnFila,
             });
 
             $scope.visitorTeamMessages.push(message);
         }
 
-        message = quiniela.ANALYZER_MESSAGES.VISITOR.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.MORE_LOSES;
+        /* message = quiniela.ANALYZER_MESSAGES.VISITOR.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.MORE_LOSES;
 
         if (fila.victoriasLocales > fila.victoriasVisitantes && fila.victoriasVisitantes > fila.empates && fila.empates > 0) {
             message = $scope.getCustomMessage(message, {
