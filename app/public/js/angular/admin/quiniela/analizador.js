b4fe22e44aaf6f88e7ff52088fad49aa23c43700
Juanma <juanmalp1992@gmail.com>
2020-10-22 21:19:33 +0200

    #537 Implementación de análisis de partido en la fila indicada con más victorias locales que empates y más empates que victorias visitantes

--- app/public/js/angular/admin/quiniela/analizador.js
+++ app/public/js/angular/admin/quiniela/analizador.js
@@ -2543,7 +2543,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        /* message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.MORE_DRAWS;
+        message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.MORE_LOCAL_WINS.MORE_DRAWS;
 
         if (fila.victoriasLocales > fila.empates && fila.empates > fila.victoriasVisitantes && fila.victoriasVisitantes > 0) {
             message = $scope.getCustomMessage(message, {
@@ -2561,7 +2561,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.SAME;
+        /* message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.MORE_WINS.SAME;
 
         if (fila.victoriasLocales > fila.empates && fila.empates === fila.victoriasVisitantes && fila.victoriasVisitantes > 0) {
             message = $scope.getCustomMessage(message, {
--- app/public/js/angular/services/quiniela.service.js
+++ app/public/js/angular/services/quiniela.service.js
@@ -210,6 +210,11 @@
                         MORE_DRAWS_THAN_VISITOR_WINS: 'En los enfrentamientos entre <b>{localTeam}</b> y <b>{visitorTeam}</b> en la fila <b>{row}</b> no se han producido victorias locales y hay más empates que victorias visitantes: <b>{numWinsLocal}</b>V (<b>{perWinsLocal}%</b>) del equipo <b>{localTeam}</b>, <b>{numDraws}</b>E (<b>{perDraws}%</b>) y <b>{numWinsVisitor}</b>V (<b>{perWinsVisitor}%</b>) del equipo <b>{visitorTeam}</b>.',
                         SAME: 'En los enfrentamientos entre <b>{localTeam}</b> y <b>{visitorTeam}</b> en la fila <b>{row}</b> no se han producido victorias locales y hay los mismos empates que victorias visitantes: <b>{numWinsLocal}</b>V (<b>{perWinsLocal}%</b>) del equipo <b>{localTeam}</b>, <b>{numDraws}</b>E (<b>{perDraws}%</b>) y <b>{numWinsVisitor}</b>V (<b>{perWinsVisitor}%</b>) del equipo <b>{visitorTeam}</b>.',
                         MORE_VISITOR_WINS_THAN_DRAWS: 'En los enfrentamientos entre <b>{localTeam}</b> y <b>{visitorTeam}</b> en la fila <b>{row}</b> no se han producido victorias locales y hay más victorias visitantes que empates: <b>{numWinsLocal}</b>V (<b>{perWinsLocal}%</b>) del equipo <b>{localTeam}</b>, <b>{numDraws}</b>E (<b>{perDraws}%</b>) y <b>{numWinsVisitor}</b>V (<b>{perWinsVisitor}%</b>) del equipo <b>{visitorTeam}</b>.',
+                    },
+                    LOCAL_WINS_DRAWS_AND_VISITOR_WINS: {
+                        MORE_LOCAL_WINS: {
+                            MORE_DRAWS: 'En los enfrentamientos entre <b>{localTeam}</b> y <b>{visitorTeam}</b> en la fila <b>{row}</b> hay más victorias locales que empates y más empates que victorias visitantes: <b>{numWinsLocal}</b>V (<b>{perWinsLocal}%</b>) del equipo <b>{localTeam}</b>, <b>{numDraws}</b>E (<b>{perDraws}%</b>) y <b>{numWinsVisitor}</b>V (<b>{perWinsVisitor}%</b>) del equipo <b>{visitorTeam}</b>.',
+                        }
                     }
                 }
             }
