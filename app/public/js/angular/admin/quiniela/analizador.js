90403a87e0cd04ca3491dd9898f063e84d5acb9f
Juanma <juanmalp1992@gmail.com>
2020-10-23 20:41:17 +0200

    #537 Implementación de análisis de partido en la fila indicada con mismas victorias locales que victorias visitantes, siendo más que los empates

--- app/public/js/angular/admin/quiniela/analizador.js
+++ app/public/js/angular/admin/quiniela/analizador.js
@@ -2723,7 +2723,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        /* message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.SAME_WINS_AND_LOSES;
+        message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.SAME_LOCAL_WINS_AND_VISITOR_WINS;
 
         if (fila.victoriasLocales === fila.victoriasVisitantes && fila.victoriasVisitantes > fila.empates && fila.empates > 0) {
             message = $scope.getCustomMessage(message, {
@@ -2741,7 +2741,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.SAME_DRAWS_AND_LOSES;
+        /* message = quiniela.ANALYZER_MESSAGES.MATCH.ROW.WINS_DRAWS_AND_LOSES.SAME_DRAWS_AND_LOSES;
 
         if (fila.empates === fila.victoriasVisitantes && fila.victoriasVisitantes > fila.victoriasLocales && fila.victoriasLocales > 0) {
             message = $scope.getCustomMessage(message, {
