fdf1948ab24f0e26c101a9f1128062122e29a7b1
Juanma <juanmalp1992@gmail.com>
2020-10-26 00:08:07 +0100

    #537 Implementación de análisis histórico de partido en competición en la fila indicada, teniendo más victorias visitantes que empates y más empates que victorias locales

--- app/public/js/angular/admin/quiniela/analizador.js
+++ app/public/js/angular/admin/quiniela/analizador.js
@@ -3252,7 +3252,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        /* message = quiniela.ANALYZER_MESSAGES.MATCH.COMPETITION.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.MORE_VISITOR_WINS.MORE_DRAWS;
+        message = quiniela.ANALYZER_MESSAGES.MATCH.COMPETITION.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.MORE_VISITOR_WINS.MORE_DRAWS;
 
         if (fila.victoriasVisitantes > fila.empates && fila.empates > fila.victoriasLocales && fila.victoriasLocales > 0) {
             message = $scope.getCustomMessage(message, {
@@ -3271,7 +3271,7 @@ function Controller($scope, $q, $window, quiniela, $sce) {
             $scope.matchMessages.push(message);
         }
 
-        message = quiniela.ANALYZER_MESSAGES.MATCH.COMPETITION.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.SAME_LOCAL_WINS_AND_DRAWS;
+        /* message = quiniela.ANALYZER_MESSAGES.MATCH.COMPETITION.ROW.LOCAL_WINS_DRAWS_AND_VISITOR_WINS.SAME_LOCAL_WINS_AND_DRAWS;
 
         if (fila.victoriasLocales === fila.empates && fila.empates > fila.victoriasVisitantes && fila.victoriasVisitantes > 0) {
             message = $scope.getCustomMessage(message, {
